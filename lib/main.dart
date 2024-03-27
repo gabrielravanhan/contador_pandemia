4acc2dc5afab89483c1516a6981b3ea781a3f739
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-03-27 20:09:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class _HomePageState extends State<HomePage> {
     print(count);
   }
 
+  bool get isEmpty => count == 0;
+
+  bool get isFull => count == 20;
+
   void increment() {
     setState(() {
       count++;
@@ -42,73 +46,74 @@ class _HomePageState extends State<HomePage> {
     print('build');
     return Scaffold(
         body: Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background.png'))),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          const SizedBox(height: 40),
-          Text(
-            '$count',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 40),
-          Row(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'))),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  fixedSize: const Size(100, 100),
-                  foregroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
+              Text(
+                isFull ? 'LOTADO' : 'Pode entrar',
+                style: TextStyle(
+                  fontSize: isFull ? 35 : 30,
+                  color: isFull ? Colors.red : Colors.black,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-              const SizedBox(width: 32),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  fixedSize: const Size(100, 100),
-                  foregroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
+              const SizedBox(height: 40),
+              Text(
+                '$count',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: isFull ? Colors.red : Colors.black,
                 ),
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: isEmpty ? null : decrement,
+                    style: TextButton.styleFrom(
+                      backgroundColor:
+                      isEmpty ? Colors.white.withOpacity(0.2) : Colors.red,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    child: const Text(
+                      'Saiu',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 32),
+                  TextButton(
+                    onPressed: isFull ? null : increment,
+                    style: TextButton.styleFrom(
+                      backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.green,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    child: const Text(
+                      'Entrou',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
